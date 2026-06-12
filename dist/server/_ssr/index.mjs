f3b43477c12a8aa50234746c9dd43943cfb9bbe6
Allen <allenuwe16@gmail.com>
2026-06-12 15:44:30 +0530

    logo updated

--- dist/server/_ssr/index.mjs
+++ dist/server/_ssr/index.mjs
@@ -53,7 +53,7 @@ function renderErrorPage() {
 let serverEntryPromise;
 async function getServerEntry() {
   if (!serverEntryPromise) {
-    serverEntryPromise = import("./server-DfO5Qzd7.mjs").then((n) => n.s).then(
+    serverEntryPromise = import("./server-YIGh4UsF.mjs").then((n) => n.s).then(
       (m) => m.default ?? m
     );
   }
